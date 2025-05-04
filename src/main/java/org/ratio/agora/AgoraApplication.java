ee007ba8d819885fba089439c2f389e8becf759b
yyubin <hazing120@gmail.com>
2025-05-04 23:33:21 +0900

    chore: initialize project

---
+++ src/main/java/org/ratio/agora/AgoraApplication.java
@@ -0,0 +1,13 @@
+package org.ratio.agora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ratio/agora/AgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ratio.agora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
